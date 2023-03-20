bc8aff4fe1dfde1dddf3d3a3dc2b1bd834a703e9
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-03-19 18:14:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entering point
+ *
+ * Return: just lower 
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
