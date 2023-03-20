3b957f9b8a393248ab0f5788be3d775b3cdfd657
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-03-20 06:06:38 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entering point 
+ *
+ * Return: just 0(success)
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0';  d <= 'f'; d++)
+	purchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	purchar(low);
+	printf("\n");
+
+	return (0);
+}
