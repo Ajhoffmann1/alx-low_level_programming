6e6538bf3cc1a2665c53964fdea8fee417dc9bf6
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-03-19 18:27:30 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * Return 1 (great)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korppar, 2015-10-19\n", stdout);
+
+	return (1);
+}
