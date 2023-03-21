717d44dbf0997aa9cb5d24ab1c308596de2422fc
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-03-20 17:47:58 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - D,F prints other types on computer and complied
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char d;
+	int e;
+	long int f;
+	long long int g;
+	float h;
+
+	printf("size oof char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(g));
+	printf("size of the float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
