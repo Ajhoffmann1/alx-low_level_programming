4bc2b36d10265264dd8642acd87f49ac1b045d7f
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-03-20 17:30:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using pf function 
- * Return: 0 (great)
+ * main - A program that print a line using of function
+ * Return: 0(great)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
-}	
+	return (0);
+}
