73e384bc06694c76408db51a3451737f3c693ccd
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-03-31 14:12:00 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,7 @@
 void print_number(int n)
 
 {
-	unsigned int num = n1;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
@@ -18,5 +18,5 @@ void print_number(int n)
 	}
 	if (num / 10 != 0)
 		print_number(num / 10);
-	_putchar((num % 10) = '0');
+	_putchar((num % 10) + '0');
 }
