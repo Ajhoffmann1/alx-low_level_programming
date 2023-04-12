78cf721d15ff510d7400e584eda0484000dfc21e
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-04-12 11:23:59 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - convert the programs passed to the program to string
+ * @ac: the argument count
+ * @av: the argument vecto
+ * Return: ...
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int yy = 0, l = 0, e = 0, o = 0;
+	char *v;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (l < ac)
+	{
+		while (av[l][e])
+		{
+			yy++;
+			e++;
+		}
+		e = 0;
+		l++;
+	}
+	v = malloc(sizeof(char) * yy) + ac + 1);
+	l = 0;
+	while (av[l][e])
+	{
+		while (av[l][e])
+		{
+			v[o] = av[l][e];
+			o++;
+			e++;
+		}
+		v[o] = '\n';
+		e = 0;
+		o++;
+		l++;
+	}
+	o++;
+	v[o] = '\0';
+	return (v);
+}
