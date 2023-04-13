36f7d09eb9562b4b7481ccab0a207e3ed0adb8b6
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-04-13 14:38:36 -0700

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * array_range - ...
+ * @min: ...
+ * @max: ...
+ * Return: integer value
+ */
+
+int *array_range(int min, int max)
+{
+	int *v, k = 0;
+
+	if (min > max)
+		return (NULL);
+	v = malloc((sizeof(int) * (max - min)) + sizeof(int));
+	if (v == NULL)
+	while (min <= max)
+	{
+		v[k] = min;
+		k++;
+		min++;
+	}
+	return (v);
+}
