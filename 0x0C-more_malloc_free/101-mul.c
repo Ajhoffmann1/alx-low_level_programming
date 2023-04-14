a0c37e7798e92e77a4de1691d09edb7300d3908e
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-04-14 12:13:56 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,9 +20,7 @@ int is_digit(char *s)
 	{
 
 		if (s[i] < '0' || s[i] > '9')
-
 			return (0);
-		
 		i++;
 
 	}
@@ -61,9 +59,9 @@ void errors(void)
 	exit(98);
 }
 
-/** 
+/**
  * main - multiplies two positive numbers
- * @aargc: number of arguments
+ * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: always 0 (success)
@@ -86,42 +84,34 @@ int main(int argc, char *argv[])
 	len = len1 + len2 + 1;
 
 	result = malloc(sizeof(int) * len);
-	 if (!result)
-		 return (1);
-
-	 for (i = 0; i <= len1 + len2; i++)
-		 result[i] = 0;
-	 for (len1 = len1 - 1; len1 >= 0; len1--)
-	 {
-		 digit1 = s1[len1] - '0';
-		 carry = 0;
-
-		 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		 {
-			 digit2 = s2[len2] - '0';
-			 carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			 result[len1 + len2 + 1] = carry % 10;
-			 carry /= 10;
-
-		 }
-
-		 if (carry > 0)
-
-			 result[len1 + len2 + 1] += carry;
-		 }
-	 for (i = 0; i < len - 1; i++)
-	 {
-
-		 if (result[i])
-
-			 a = 1;
-		 if (a)
-
-			 _putchar(result[i] + '0');
-	 }
-	 if (!a)
-		 _putchar('0');
-	 _putchar('\n');
-	 free(result);
-	 return (0);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('\0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
