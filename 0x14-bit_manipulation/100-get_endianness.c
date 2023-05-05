1a0b95586a482d2d115d46e7b31157726149273f
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-05-05 08:26:40 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,14 @@
-Hol
+#include "main.h"
+
+/**
+ * get_endianness - there is a check if its little
+ * Return:0 big, 1 small
+ */
+
+int get_endianness(void)
+{
+	unsigned int love_is_coding = 1;
+	char *car = (char *) &love_is_coding;
+
+	return (*car);
+}
