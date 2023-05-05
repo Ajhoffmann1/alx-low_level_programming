33d1e3a37fa162ed19adfbe6ea075c10832d566d
Ajhoffmann1 <ajhoffmann.2009@aol.com>
2023-05-05 07:41:51 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * flip_bits - the bits to be changed has a check number
- * from one number to others
- * @n: numbers start
- * @m: numbers ens
- * Return: the numbers in the bit to substitute
+ * clear_bit - a set to a given bit to 0 from the value of a bit
+ * @n: a pointer to substitute the number to
+ * @index: the bit is cleared by an index
+ *
+ * Return: 1 success, -1 fail.
  */
 
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{	int l, rounder = 0;
-	unsigned long int previous;
-	unsigned long int ex = n ^ m;
-
-	for (l = 63; l >= 0; l--)
-	{
-		previous = ex >> l;
-		if (precious & 1)
-			rounder++;
-	}
-	return (rounder);
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = (~(1UL << index) & *n);
+	return (1);
 }
